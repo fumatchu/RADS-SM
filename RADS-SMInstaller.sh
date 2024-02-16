9daea8c59a984cb4f1d87e99e1ace38b053eea5b
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-02-16 08:26:58 -0500

    Create RADS-SMInstaller.sh

---
+++ RADS-SMInstaller.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#RADS-SMInstaller.sh #Bootstrap to GIT REPO
+cat <<EOF
+**************************
+Please wait while we gather some files
+**************************
+
+
+Installing wget and git
+EOF
+sleep 1
+
+dnf -y install wget git 
+
+cat <<EOF
+*****************************
+Retrieving Files from GitHub
+*****************************
+EOF
+
+sleep 1
+
+mkdir /root/RADS-SMInstaller
+
+git clone https://github.com/fumatchu/RADS-SM.git /root/RADS-SMInstaller
+
+chmod 700 /root/RADS-SMInstaller/RA*
+clear
+
+/root/RADS-SMInstaller/RADS-SMInstall.sh
