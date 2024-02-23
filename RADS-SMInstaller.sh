2f9aa74a4d7affb65034ba762ce64019f7c540aa
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-02-23 16:12:15 -0500

    Update RADS-SMInstaller.sh

--- RADS-SMInstaller.sh
+++ RADS-SMInstaller.sh
@@ -1,24 +1,46 @@
 #!/bin/sh
 #RADS-SMInstaller.sh #Bootstrap to GIT REPO
+TEXTRESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+USER=$(whoami)
+MAJOROS=$(cat /etc/redhat-release | grep -Eo "[0-9]" | sed '$d')
+
+#Checking for user permissions
+if [ "$USER" = "root" ]; then
+echo " "
+else
+  echo ${RED}"This program must be run as root ${TEXTRESET}"
+  echo "Exiting"
+fi
+#Checking for version Information
+if [ "$MAJOROS" = "9" ]; then
+echo " "
+else
+  echo ${RED}"Sorry, but this installer only works on Rocky 9.X ${TEXTRESET}"
+  echo "Please upgrade to ${GREEN}Rocky 9.x${TEXTRESET}"
+  echo "Exiting the installer..."
+  exit
+fi
+
 cat <<EOF
-**************************
+${GREEN}**************************
 Please wait while we gather some files
-**************************
+**************************${TEXTRESET}
 
 
-Installing wget and git
+${YELLOW}Installing wget and git${TEXTRESET}
 EOF
 sleep 1
 
-dnf -y install wget git 
+dnf -y install wget git
 
 cat <<EOF
-*****************************
+${YELLOW}*****************************
 Retrieving Files from GitHub
-*****************************
+*****************************${TEXTRESET}
 EOF
-
-sleep 1
 
 mkdir /root/RADS-SMInstaller
 
