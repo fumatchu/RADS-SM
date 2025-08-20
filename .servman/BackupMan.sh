f78dbe7a97edc135ac89fe39cde9e7c17d157196
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2025-08-20 09:47:15 -0400

    Create BackupMan.sh

---
+++ .servman/BackupMan.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+user=$(whoami)
+#Checking for user permissions
+if [ "$user" != "root" ]; then
+    echo ${red}"This program must be run as root ${textreset}"
+    echo "Exiting"
+    exit
+else
+    echo "Running Program"
+fi
+
+items=(1 "Backup Local"
+       2 "Backup Remote"
+       3 "Backup Local AND Remote"
+       4 "Restore Backup"
+)
+
+while choice=$(dialog --title "$TITLE" \
+    --backtitle "Samba Backup and Restore" \
+    --menu "Please select" 25 40 3 "${items[@]}" \
+    2>&1 >/dev/tty); do
+    case $choice in
+    1) /root/.servman/BackupMan/backup-local.sh ;;
+    2) /root/.servman/BackupMan/backup-remote.sh ;;
+    3) /root/.servman/BackupMan/backup-local-remote.sh ;;
+    4) /root/.servman/BackupMan/restore.sh ;;
+    esac
+done
+clear # clear after user pressed Cancel
